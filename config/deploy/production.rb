be44019f6d18c4b127f7e91c6f678d8d8e7b1702
Martin <hongzeqin@gmail.com>
2015-05-29 17:24:25 +0800

    rollback config for production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@112.74.87.114}
-role :web, %w{deploy@112.74.87.114}
-role :db,  %w{deploy@112.74.87.114}
+role :app, %w{martin@jianggaowang.com}
+role :web, %w{martin@jianggaowang.com}
+role :db,  %w{martin@jianggaowang.com}
 
 
 # Extended Server Syntax

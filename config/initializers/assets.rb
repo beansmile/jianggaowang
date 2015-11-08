88f1707f75ddc446a820623446dc1e355a7d67d4
Martin Hong <hongzeqin@gmail.com>
2015-11-08 21:44:15 +0800

    check_flat.png should be checked out into assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( jquery.lazyload.js mobile.js mobile.css galleria.classic.css galleria/classic/* photoswipe/default-skin/* )
+Rails.application.config.assets.precompile += %w( jquery.lazyload.js mobile.js
+  mobile.css galleria.classic.css galleria/classic/* photoswipe/default-skin/*
+  check_flat.png )

4eb266173a57a16ec05692c3319a16e0b1393b98
david <lexuszhi1990@gmail.com>
2015-12-12 23:41:17 +0800

    add done for slide

--- app/jobs/slide_convert_job.rb
+++ app/jobs/slide_convert_job.rb
@@ -3,7 +3,7 @@ class SlideConvertJob < ActiveJob::Base
 
   def perform(slide_id)
     slide = Slide.find slide_id
-    puts "start to convert slide #{slide.id}"
     slide.convert!
+    slide.done!
   end
 end

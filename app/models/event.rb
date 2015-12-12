cd05f91cb2dba24ee7ba5e4fdff44ef0240e7616
david <lexuszhi1990@gmail.com>
2015-12-12 19:36:13 +0800

    add Event uploader

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
+
+  # Attr related macros
+  mount_uploader :cover, EventUploader
+
   # Associations
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_and_belongs_to_many :slides

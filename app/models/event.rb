912cc29e6d776eeb3a8bce41b2fdb783e0cff975
david <lexuszhi1990@gmail.com>
2015-12-14 19:59:52 +0800

    add scope for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,9 @@ class Event < ActiveRecord::Base
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_many :slides
 
+  # scope
+  scope :order_by_created_at_desc, -> { order(created_at: :desc) }
+
   def start_time
     start_at.strftime("%m/%d/%Y") if start_at
   end

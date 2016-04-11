5491be19f4978580a575c879356e32502cb248f9
zhaqiang <hugochougt@gmail.com>
2016-04-11 10:16:50 +0800

    Update email notification
    https://trello.com/c/t2bX04wL/42-2-update-email-notification

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,8 +1,10 @@
 Rails.application.config.action_mailer.delivery_method = :smtp
 Rails.application.config.action_mailer.smtp_settings = {
-  address:              'smtp.163.com',
-  port:                 25,
-  user_name:            Rails.application.secrets[:mail]["user_name"],
-  password:             Rails.application.secrets[:mail]["password"],
-  authentication:       'plain'
+  address:              'smtp.exmail.qq.com',
+  port:                 465,
+  user_name:            Rails.application.secrets.mail['user_name'],
+  password:             Rails.application.secrets.mail['password'],
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  tls: true
 }

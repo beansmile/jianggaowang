3165c09929cc2ce368a594bdfe96be8924ed8c59
zavven <zavven.z@gmail.com>
2016-04-19 12:34:13 +0800

    update the jianggaowang.js

--- app/assets/javascripts/jianggaowang.js
+++ app/assets/javascripts/jianggaowang.js
@@ -1,3 +1,6 @@
 //= require ./frontend/app
+//= require ./frontend/common
 //= require ./frontend/events
+//= require ./frontend/profile
+//= require ./frontend/slides
 //= require ./frontend/carousel

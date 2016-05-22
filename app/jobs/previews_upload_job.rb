c0c534897b4a42c0dba7302d72a65e647b4f3e23
Martin Hong <hongzeqin@gmail.com>
2016-05-22 10:07:47 +0800

    implement the upload jobs

---
+++ app/jobs/previews_upload_job.rb
@@ -0,0 +1,31 @@
+class PreviewsUploadJob < ActiveJob::Base
+  queue_as :uploads
+
+  def perform(slide_id)
+    slide = Slide.find slide_id
+    if slide.status == "done" && (previews = slide.previews).any?
+      previews.each do |preview|
+        remote_file_path = Pathname.new(preview.file.path).relative_path_from(Rails.root)
+
+        code, result, response_headers = Qiniu::Storage.upload_with_token_2(
+          upload_token,
+          preview.file.path,
+          remote_file_path
+        )
+
+        preview.update(
+          bucket_domain: Qiniu::Config.settings[:bucket_domain],
+          bucket: Qiniu::Config.settings[:bucket],
+          remote_file_path: remote_file_path
+        )
+      end
+    end
+  end
+
+  def upload_token
+    return @upload_token if @upload_token
+
+    put_policy = Qiniu::Auth::PutPolicy.new(Qiniu::Config.settings[:bucket], nil, 3600)
+    @upload_token = Qiniu::Auth.generate_uptoken(put_policy)
+  end
+end

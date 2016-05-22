00661909044b804b63b2cb28e21912c8c3e99104
Martin Hong <hongzeqin@gmail.com>
2016-05-22 10:14:43 +0800

    enable to upload previews

--- app/jobs/previews_upload_job.rb
+++ app/jobs/previews_upload_job.rb
@@ -7,17 +7,21 @@ class PreviewsUploadJob < ActiveJob::Base
       previews.each do |preview|
         remote_file_path = Pathname.new(preview.file.path).relative_path_from(Rails.root)
 
-        code, result, response_headers = Qiniu::Storage.upload_with_token_2(
+        code, result, _ = Qiniu::Storage.upload_with_token_2(
           upload_token,
           preview.file.path,
           remote_file_path
         )
 
-        preview.update(
-          bucket_domain: Qiniu::Config.settings[:bucket_domain],
-          bucket: Qiniu::Config.settings[:bucket],
-          remote_file_path: remote_file_path
-        )
+        if code.to_s == "200"
+          preview.update(
+            bucket_domain: Qiniu::Config.settings[:bucket_domain],
+            bucket: Qiniu::Config.settings[:bucket],
+            remote_file_path: result["key"]
+          )
+        else
+          raise "Upload Failed"
+        end
       end
     end
   end
